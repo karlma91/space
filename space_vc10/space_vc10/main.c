91c7e1b3c9d117e112e4830a76eccf4d5a34a184
animage <none@none>
2012-11-19 20:59:53 +0100

    added msvc project

---
+++ space_vc10/space_vc10/main.c
@@ -0,0 +1,13 @@
+#include "SDL.h"
+#include "chipmunk/chipmunk.h"
+
+int main( int argc, char* args[] )
+{
+    //Start SDL
+    SDL_Init( SDL_INIT_EVERYTHING );
+    
+    //Quit SDL
+    SDL_Quit();
+    
+    return 0;
+}
