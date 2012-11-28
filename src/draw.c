4c19ccd7e22a373267909f0e3fb9a78ecf33885d
animage <none@none>
2012-11-28 14:00:12 +0100

    added reusable color-array and made second gradient color visible (dark red)

--- src/draw.c
+++ src/draw.c
@@ -5,24 +5,37 @@ static GLfloat circle_16[CIRCLE_MEDIUM];
 static GLfloat circle_64[CIRCLE_BIG];
 static GLfloat circle_128[CIRCLE_EXTRA];
 
+static GLfloat colors1[CIRCLE_EXTRA/2*3];
+
 static void init_array(int size, GLfloat *array);
+
+int i, len;
 
 void draw_init(){
 	init_array( CIRCLE_SMALL, circle_8);
 	init_array(CIRCLE_MEDIUM, circle_16);
 	init_array(CIRCLE_BIG, circle_64);
 	init_array(CIRCLE_EXTRA, circle_128);
+	
+	colors1[0] = 0.9f;
+	colors1[1] = 0.1f;
+	colors1[2] = 0.1f;
+	
+	for(i=3;i<CIRCLE_EXTRA/2*3;i+=3){
+		colors1[i]= 0.2f;
+		colors1[i+1]= 0.1f;
+		colors1[i+2]= 0.1f;
+	}
 }
 
 static void 
 init_array(int size, GLfloat *array)
 {
-  int i;
   array[0]=0.0f;
   array[1]=0.0f;
-  for(i = 0; i < size-3; i++){
-    array[i+2] = sinf( 2*M_PI*i / (size-6));
-    array[i+3] = cosf( 2*M_PI*i / (size-6));
+  for(i = 2; i < size-1; i++){
+    array[i] = sinf( 2*M_PI*i / (size-6));
+    array[i+1] = cosf( 2*M_PI*i / (size-6));
     i++;
   }
   array[size-2] = 0.0f;
@@ -31,7 +44,6 @@ init_array(int size, GLfloat *array)
 
 void drawCircle(cpVect center, cpFloat angle, cpFloat radius, Color fill, Color line)
 {
-	int len,i;
 	GLfloat *array;
 	if(radius < 10){
 		len = CIRCLE_SMALL/2;
@@ -49,15 +61,6 @@ void drawCircle(cpVect center, cpFloat angle, cpFloat radius, Color fill, Color 
 	
   //TMP
   glEnableClientState(GL_COLOR_ARRAY);
-  GLfloat colors1[len*3]; //= {fill.r, fill.g, fill.b};
-  colors1[0] = 1.0f;
-  colors1[1] = 0.0f;
-  colors1[2] = 0.0f;
-  for(i=3;i<len*3;i+=3){
-	colors1[i]= 0.0f;
-	colors1[i+1]= 0.0f;
-	colors1[i+2]= 0.0f;
-  }
   
   glVertexPointer(2, GL_FLOAT, 0, array);
   glColorPointer(3, GL_FLOAT, 0, colors1);
@@ -71,12 +74,15 @@ void drawCircle(cpVect center, cpFloat angle, cpFloat radius, Color fill, Color 
       glColor_from_color(fill);
       glDrawArrays(GL_TRIANGLE_FAN, 0, len-1);
     }
-    /*          
+             
     if(line.a > 0){
-     glColor_from_color(line);
-     glDrawArrays(GL_LINE_STRIP, 0, len);
+	  glColor_from_color(line);
+	  glBegin(GL_LINES);
+	    glVertex2f(0,0);
+	    glVertex2f(1,0);
+	  glEnd();
     }
-    */
+    
     } glPopMatrix();
   
   glDisableClientState(GL_COLOR_ARRAY);
