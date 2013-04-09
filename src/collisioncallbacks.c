529d3e37eb9da15e32797b745fe6750ac04ae365
Karlma <none@none>
2013-04-09 13:17:54 +0200

    fixed crash when player died

--- src/collisioncallbacks.c
+++ src/collisioncallbacks.c
@@ -4,16 +4,17 @@
 #include "particles.h"
 #include "space.h"
 
-static int collision_object_bullet(cpArbiter *arb, cpSpace *space, void *unused);
+static int collision_object_bullet_with_score(cpArbiter *arb, cpSpace *space, void *unused);
 static void callback_bullet_ground(cpArbiter *arb, cpSpace *space, void *unused);
 static void collision_player_object(cpArbiter *arb, cpSpace *space, void *unused);
 static void se_add_explotion_at_contact_point(cpArbiter *arb);
 static void add_sparks_at_contactpoint(cpArbiter *arb);
+static int collision_object_bullet(cpArbiter *arb, cpSpace *space, void *unused);
 
 void collisioncallbacks_init()
 {
-	cpSpaceAddCollisionHandler(space, ID_TANK, ID_BULLET_PLAYER, collision_object_bullet, NULL, NULL, NULL, NULL);
-	cpSpaceAddCollisionHandler(space, ID_TANK_FACTORY, ID_BULLET_PLAYER, collision_object_bullet, NULL, NULL, NULL, NULL);
+	cpSpaceAddCollisionHandler(space, ID_TANK, ID_BULLET_PLAYER, collision_object_bullet_with_score, NULL, NULL, NULL, NULL);
+	cpSpaceAddCollisionHandler(space, ID_TANK_FACTORY, ID_BULLET_PLAYER, collision_object_bullet_with_score, NULL, NULL, NULL, NULL);
 	cpSpaceAddCollisionHandler(space, ID_BULLET_PLAYER, ID_GROUND, NULL, NULL, callback_bullet_ground, NULL, NULL);
 	cpSpaceAddCollisionHandler(space, ID_BULLET_ENEMY, ID_GROUND, NULL, NULL, callback_bullet_ground, NULL, NULL);
 
@@ -22,7 +23,7 @@ void collisioncallbacks_init()
 	cpSpaceAddCollisionHandler(space, ID_PLAYER, ID_TANK_FACTORY, NULL, NULL, collision_player_object, NULL, NULL);
 }
 
-static int collision_object_bullet(cpArbiter *arb, cpSpace *space, void *unused)
+static int collision_object_bullet_with_score(cpArbiter *arb, cpSpace *space, void *unused)
 {
 	cpShape *a, *b;
 	cpArbiterGetShapes(arb, &a, &b);
@@ -39,6 +40,24 @@ static int collision_object_bullet(cpArbiter *arb, cpSpace *space, void *unused)
 		se_add_score_and_popup(b->body->p, *(object->components.score));
 	}
 	//cpSpaceAddPostStepCallback(space, (cpPostStepFunc)postStepRemove, a, NULL);
+
+	return 0;
+}
+
+static int collision_object_bullet(cpArbiter *arb, cpSpace *space, void *unused)
+{
+	cpShape *a, *b;
+	cpArbiterGetShapes(arb, &a, &b);
+	object_data *object = (object_data *)(a->body->data);
+	object_data *bullet = (object_data*)(b->body->data);
+
+	bullet->alive = 0;
+
+	se_add_explotion_at_contact_point(arb);
+
+	if (se_damage_object(object, *(bullet->components.damage))) {
+		particles_get_emitter_at(EMITTER_EXPLOSION, b->body->p);
+	}
 
 	return 0;
 }
