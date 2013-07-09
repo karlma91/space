0652f37cebea7f6cdf2b3ab13718e344fb8829d7
Karlma <none@none>
2013-07-09 18:25:26 +0200

    started on bmfont
    --HG--
    branch : supporting iOS

--- src/bmfont.c
+++ src/bmfont.c
@@ -1,8 +1,30 @@
-/*
- * bmfont.c
- *
- *  Created on: 5. juli 2013
- *      Author: Karl
- */
 
+
+
+
+typedef struct _bm_char{
+    int id;
+    int x;
+    int y;
+    int w;
+    int h;
+    int x_offset;
+    int y_offset;
+    int x_advance;
+} bm_char;
+
+
+struct bm_font{
+    float        scale;
+    int          base, line_h, w, h;
+    int          tex_id;
+    int          base_list;
+    bm_char      chars[256];
+    float        mmat[16];
+};
+
+int bmfont_read_font(char *filename)
+{
+
+}
 
