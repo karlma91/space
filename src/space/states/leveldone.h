405937a887f49edb4582f494a81e8c1f6511a043
animage <none@none>
2013-07-24 11:55:34 +0200

    forgot to add file
    --HG--
    branch : supporting iOS

---
+++ src/space/states/leveldone.h
@@ -0,0 +1,15 @@
+/*
+ * leveldone.h
+ *
+ *  Created on: 23. juli 2013
+ *      Author: Mathias
+ */
+
+#ifndef LEVELDONE_H_
+#define LEVELDONE_H_
+
+extern void leveldone_init();
+
+void leveldone_status(int stars, int score);
+
+#endif /* LEVELDONE_H_ */
