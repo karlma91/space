5a1f9a01a0255ebf48e0409b5bb715da83ed3d87
karlmka <none@none>
2013-09-20 12:32:52 +0200

    added simple debug draw for cpSpace

---
+++ src/engine/testing/debugdraw.c
@@ -0,0 +1,64 @@
+
+#include "chipmunk_private.h"
+#include "chipmunk.h"
+#include "debugdraw.h"
+#include "we_graphics.h"
+
+static void circle_line(cpVect p, float r);
+
+static void DrawShape(cpShape *shape, void * unused)
+{
+    cpBody *body = shape->body;
+
+    draw_color(COL_RED);
+    switch(shape->klass->type){
+        case CP_CIRCLE_SHAPE: {
+            cpCircleShape *circle = (cpCircleShape *)shape;
+            circle_line(circle->tc, circle->r);
+            break;
+        }
+        case CP_SEGMENT_SHAPE: {
+            cpSegmentShape *seg = (cpSegmentShape *)shape;
+            draw_quad_line(seg->ta, seg->tb, seg->r*2);
+            draw_flush_simple();
+            break;
+        }
+        case CP_POLY_SHAPE: {
+            cpPolyShape *poly = (cpPolyShape *)shape;
+            draw_push_matrix();
+            draw_translate(body->p.x, body->p.y);
+            draw_rotate(body->a);
+
+            int i = 0;
+            for(i=0; i< poly->numVerts; i++) {
+            	if(i < poly->numVerts - 1) {
+            		draw_quad_line(poly->verts[i], poly->verts[i + 1], 2);
+            	} else {
+            		draw_quad_line(poly->verts[i], poly->verts[0], 2);
+            	}
+            }
+            draw_flush_simple();
+        	draw_pop_matrix();
+
+            break;
+        }
+        default: break;
+    }
+}
+
+void debugdraw_space(cpSpace *space)
+{
+    cpSpaceEachShape(space, (cpSpaceShapeIteratorFunc)DrawShape, NULL);
+}
+
+static void circle_line(cpVect p, float r)
+{
+	static GLfloat circle[128];
+	int i=0;
+	for(i = 0; i < 128; i += 2) {
+		circle[i] = p.x + sinf( 2*M_PI*i / (128-2)) * r;
+		circle[i+1] = p.y + cosf( 2*M_PI*i / (128-2)) * r;
+	}
+	draw_vertex_pointer(2, GL_FLOAT, 0, circle);
+	draw_draw_arrays(GL_LINE_STRIP, 0, 64);
+}
