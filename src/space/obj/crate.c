35e5bcf84adf570327445b9de6a11f358e765676
karlma <none@none>
2014-01-11 20:56:33 +0100

    forgot file

---
+++ src/space/obj/crate.c
@@ -0,0 +1,64 @@
+
+#include "object_types.h"
+#include "../states/space.h"
+#include "../spaceengine.h"
+#include "../game.h"
+
+#define OBJ_NAME crate
+#include "we_defobj.h"
+#include "chipmunk_unsafe.h"
+
+static void init(OBJ_TYPE *OBJ_NAME)
+{
+	cpBodySetPos(crate->data.body, crate->data.p_start);
+}
+
+static void on_create(OBJ_TYPE *OBJ_NAME)
+{
+	COMPONENT_SET(crate, HPBAR, &crate->hp_bar);
+	COMPONENT_SET(crate, COINS, &crate->param.coins);
+	sprite_create(&(crate->data.spr), SPRITE_GEAR, crate->param.size, crate->param.size, 0);
+	SDL_Log("crate size: %f", crate->param.size );
+	float mass = 5;
+	cpFloat moment = cpMomentForBox(mass, crate->param.size, crate->param.size);
+	crate->data.body = cpSpaceAddBody(current_space, cpBodyNew(mass, moment));
+	cpBodySetPos(crate->data.body, crate->data.p_start); //FIXME
+	cpBodySetUserData(crate->data.body, (instance*) crate);
+	se_velfunc(crate->data.body, 1);
+
+	cpShape *shape = we_add_box_shape(current_space, crate->data.body, crate->param.size, crate->param.size, 0.9, 0.5);
+	cpShapeSetCollisionType(shape, &this);
+	cpShapeSetGroup(shape, crate);
+
+	hpbar_init(&(crate->hp_bar), crate->param.max_hp, 100, 25, 0, 50, &(crate->data.body->p));
+
+}
+
+static void on_update(OBJ_TYPE *OBJ_NAME)
+{
+
+}
+
+static void on_render(OBJ_TYPE *OBJ_NAME)
+{
+	draw_color(COL_WHITE);
+	float border = 50;
+	draw_quad_patch_center_spr(RLAY_GAME_MID, &(crate->data.spr),crate->data.body->p,cpv(crate->param.size-border*2, crate->param.size-border*2), border, crate->data.body->a);
+	hpbar_draw(RLAY_GUI_BACK, &crate->hp_bar, cpvtoangle(crate->data.body->p));
+}
+
+static void on_destroy(OBJ_TYPE *OBJ_NAME)
+{
+	explosion_create(crate->data.body->p, EM_EXPLOSIONBIG, EM_FRAGMENTS, SND_BUILDING_EXPLODE, 2500, 250, 0.10);
+	se_spawn_coins((instance *)crate);
+	instance_remove((instance *)crate);
+}
+
+static void on_update_dead(OBJ_TYPE *OBJ_NAME)
+{
+}
+
+static void on_remove(OBJ_TYPE *OBJ_NAME)
+{
+	we_body_remove(current_space, &crate->data.body);
+}
